c7aa9613348b9db43bf12e87479b2929ffa1b657
Tim Angus <tim@ngus.net>
2020-09-01 18:42:58 +0100

    Give matchers distinct owner names

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const path = require('path');
 const matchers = ['compiler.json', 'cppcheck.json', 'clang-tidy.json']
 
 for(const matcher of matchers)
-    console.log('::add-matcher::' + path.join(__dirname, matcher) + '\n');
+    console.log('::add-matcher::' + path.join(__dirname, matcher));
