56a426558dfb1c7e128a0b6dcd2f851eb4104209
Tim Angus <tim@ngus.net>
2020-09-01 18:05:23 +0100

    Add clang-tidy matcher

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const matchers = ['compiler.json']
+const matchers = ['compiler.json', 'clang-tidy.json']
 
 for(const matcher of matchers)
     console.log('::add-matcher::' + path.join(__dirname, matcher));
