722c8c88ef184ac597990bc738614758928f5218
Tim Angus <tim@ngus.net>
2020-09-02 14:32:53 +0100

    Add MSVC matcher

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const matchers = ['compiler.json', 'cppcheck.json', 'clang-tidy.json']
+const matchers = ['compiler.json', 'cppcheck.json', 'clang-tidy.json', 'msvc.json']
 
 for(const matcher of matchers)
     console.log('::add-matcher::' + path.join(__dirname, matcher));
