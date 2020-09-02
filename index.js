ab71fdc94263a0b5110967bcc94ef79616b8260d
Tim Angus <tim@ngus.net>
2020-09-02 16:51:40 +0100

    Add clazy matcher

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const path = require('path');
-const matchers = ['compiler.json', 'cppcheck.json', 'clang-tidy.json', 'msvc.json']
+const matchers =
+[
+    // Covers GCC and clang style errors
+    'generic.json',
+    // cppcheck default output (non-xml)
+    'cppcheck.json',
+    'clang-tidy.json',
+    // Basically a copy of generic.json, except that it looks at clazy warnings specifically
+    'clazy.json',
+    'msvc.json'
+]
 
 for(const matcher of matchers)
     console.log('::add-matcher::' + path.join(__dirname, matcher));
